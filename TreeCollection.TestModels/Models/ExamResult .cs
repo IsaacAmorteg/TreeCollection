f0b789bc59bfa09cb3c9e6d7d218ce81cbbb6a21
Isaac Amortegui <isaacamortegc@outlook.com>
2023-05-07 23:52:37 -0500

    fixing issue with nullable types

--- TreeCollection.TestModels/Models/ExamResult .cs
+++ TreeCollection.TestModels/Models/ExamResult .cs
@@ -1,8 +1,9 @@
-﻿using TreeCollection.TestModels.Enums;
+﻿using System;
+using TreeCollection.TestModels.Enums;
 
 namespace TreeCollection.TestModels.Models
 {
-    public class ExamResult
+    public class ExamResult : IComparable<ExamResult>
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -17,6 +18,31 @@ namespace TreeCollection.TestModels.Models
             Exam = exam;
             Score = score;
             Date = date;
+        }
+
+        public int CompareTo(ExamResult? other)
+        {
+            if (other == null)
+            {
+                return 1; // null is greater than non-null
+            }
+
+            // Compare based on student name first
+            int result = string.Compare(Name, other.Name, StringComparison.Ordinal);
+            if (result != 0)
+            {
+                return result;
+            }
+
+            // If names are the same, compare based on date
+            result = DateTime.Compare(Date, other.Date);
+            if (result != 0)
+            {
+                return result;
+            }
+
+            // If names and dates are the same, compare based on ID
+            return Id.CompareTo(other.Id);
         }
     }
 }
--- TreeCollection/Tree.cs
+++ TreeCollection/Tree.cs
@@ -1,28 +1,131 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace TreeCollection
 {
-    public class Tree<T> : IEnumerable<T> 
+    public class Tree<T> : IEnumerable<T> where T : IComparable<T>
     {
-
-        public Tree(bool isReversed = false) 
+        private class Node
         {
-            throw new NotImplementedException();
+            public T Value { get; set; }
+            public Node? Left { get; set; }
+            public Node? Right { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+                Left = null;
+                Right = null;
+            }
+        }
+
+        private Node? _root;
+        private bool _isReversed;
+
+        public Tree(bool isReversed = false)
+        {
+            _root = null;
+            _isReversed = isReversed;
         }
 
         public void Add(T newElement)
         {
-            throw new NotImplementedException();
+            if (_root == null)
+            {
+                _root = new Node(newElement);
+            }
+            else
+            {
+                var current = _root;
+                var parent = _root;
+
+                while (current != null)
+                {
+                    parent = current;
+
+                    if (newElement.CompareTo(current.Value) < 0)
+                    {
+                        current = current.Left;
+                    }
+                    else if (newElement.CompareTo(current.Value) > 0)
+                    {
+                        current = current.Right;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Element already exists in the tree.");
+                    }
+                }
+
+                if (newElement.CompareTo(parent.Value) < 0)
+                {
+                    parent.Left = new Node(newElement);
+                }
+                else if (newElement.CompareTo(parent.Value) > 0)
+                {
+                    parent.Right = new Node(newElement);
+                }
+            }
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            if (_isReversed)
+            {
+                foreach (var item in TraversePostOrder(_root))
+                {
+                    yield return item;
+                }
+            }
+            else
+            {
+                foreach (var item in TraverseInOrder(_root))
+                {
+                    yield return item;
+                }
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
+        }
+
+        private IEnumerable<T> TraverseInOrder(Node node)
+        {
+            if (node != null)
+            {
+                foreach (var item in TraverseInOrder(node.Left))
+                {
+                    yield return item;
+                }
+
+                yield return node.Value;
+
+                foreach (var item in TraverseInOrder(node.Right))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        private IEnumerable<T> TraversePostOrder(Node node)
+        {
+            if (node != null)
+            {
+                foreach (var item in TraversePostOrder(node.Right))
+                {
+                    yield return item;
+                }
+
+                yield return node.Value;
+
+                foreach (var item in TraversePostOrder(node.Left))
+                {
+                    yield return item;
+                }
+            }
         }
     }
 }
